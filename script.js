19359fef84c944dc1ee38f6d12b035579f0a2b49
Yigit05 <86573187+Yigit05@users.noreply.github.com>
2024-04-05 00:56:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const textElement = document.getElementById("text");
-const speedElement = document.getElementById("speed");
-const text = "İLKAYIN MEMESİ SARKIYO SALLA İLKAY MEMELERİ";
-let index = 1;
-let speed = 300 / speedElement.ariaValueMax;
-
-const writeTExt = () => {
-    textElement.innerHTML = text.slice(0, index);
-    index++;
-    if (index > text.length) index = 1;
-    setTimeout(writeTExt, speed);
-}
-writeTExt();
-
-speedElement.addEventListener("input",(e) => (speed = 300 / e.target.value));
 
